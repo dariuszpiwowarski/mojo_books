653318a5dd7d7884f1eb1a757e5e307c15a41770
Dariusz Piwowarski <piwowarskidariusz@gmail.com>
2026-01-01 18:37:31 +0100

    Return editions json

--- lib/MojoBooks/Controller/Example.pm
+++ lib/MojoBooks/Controller/Example.pm
@@ -4,8 +4,10 @@ use Mojo::Base 'Mojolicious::Controller', -signatures;
 # This action will render a template
 sub welcome ($self) {
 
-  # Render template "example/welcome.html.ep" with message
-  $self->render(json => 'Welcome to the Mojolicious real-time web framework!!!');
+	my $editions =  $self->pg->db->query('SELECT * FROM edition')->hashes->to_array;
+	$self->render(json => {
+			editions => $editions
+		});
 }
 
 1;
