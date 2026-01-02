ebb51844deb4e13fa1e83f056a0af67d93d1765b
Dariusz Piwowarski <piwowarskidariusz@gmail.com>
2026-01-02 15:18:40 +0100

    Add swagger

--- cpanfile
+++ cpanfile
@@ -1,2 +1,4 @@
 requires 'Mojolicious';
+requires 'Mojolicious::Plugin::OpenAPI';
+requires 'Mojolicious::Plugin::SwaggerUI';
 requires 'Mojo::Pg';
--- lib/MojoBooks.pm
+++ lib/MojoBooks.pm
@@ -9,20 +9,26 @@ sub startup ($self) {
   # Load configuration from config file
   my $config = $self->plugin('NotYAMLConfig');
 
+  $self->plugin('OpenAPI' => {
+      url => $self->home->rel_file('openapi.yaml')
+    });
+  $self->plugin(
+    SwaggerUI => {
+      route   => $self->routes()->any('api'),
+      url     => "/",
+      title   => "Mojo Books",
+      favicon => "/favicon-32x32.png"
+    }
+  );
+
   # Configure the application
   $self->secrets($config->{secrets});
 
-	# Pg
- 	$self->helper(pg => sub {
-		my $self = shift;
-		state $pg = Mojo::Pg->new($ENV{DATABASE_URL});
-	});
-
-  # Router
-  my $r = $self->routes;
-
-  # Normal route to controller
-  $r->get('/')->to('Example#welcome');
+  # Pg
+  $self->helper(pg => sub {
+      my $self = shift;
+      state $pg = Mojo::Pg->new($ENV{DATABASE_URL});
+    });
 }
 
 1;
